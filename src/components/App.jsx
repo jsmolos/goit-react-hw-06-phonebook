e34915eb11ff3c1e1fbc0dc0fa3099ebf709519f
jsmolos <molosj@yahoo.com>
2024-05-04 20:32:17 +0800

    reduxify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact } from '../redux/contactsSlice';
+import { setFilter } from '../redux/filterSlice';
+import { getContacts, getFilter } from '../redux/selectors';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const handleAddContact = newContact => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleSetFilter = newFilter => {
+    dispatch(setFilter(newFilter));
+  };
+
+  
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={handleAddContact} contacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={handleSetFilter} />
+      <ContactList
+        contacts={filteredContacts} 
+        deleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
